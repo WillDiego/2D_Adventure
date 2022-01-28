7f8c979d5226341fd56f9850f21c27b7c25a9d02
Will Diego <willdiego@gmail.com>
2022-01-28 13:54:51 -0800

    added sound

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -25,17 +25,18 @@ public class GamePanel extends JPanel implements  Runnable {
     //WORLD SETTINGS
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
 
     //how fast the game updates
     int FPS = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler(this);
-    Thread gameThread;
+    Sound sound = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    Thread gameThread;
+
+    //ENTITY AND OBJECT
     public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10]; //10 = how many objects displayed at once (only 10 cause more = slow)
 
@@ -50,6 +51,8 @@ public class GamePanel extends JPanel implements  Runnable {
 
     public void setupGame() {
         aSetter.setObject();
+
+        playMusic(5);
     }
 
 //    public void zoomInOut(int i) {
@@ -155,5 +158,19 @@ public class GamePanel extends JPanel implements  Runnable {
         player.draw(g2);
 
         g2.dispose();
+    }
+    public void playMusic(int i) {
+        sound.setFile(i);
+        sound.play();
+        sound.loop();
+    }
+    public void stopMusic() {
+
+        sound.stop();
+    }
+    public void playSE(int i) {
+
+        sound.setFile(i);
+        sound.play();
     }
 }
